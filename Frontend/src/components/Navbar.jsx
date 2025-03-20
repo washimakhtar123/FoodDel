8ed4e858b913ae4e35d866b74e60c6a4e5ed7eaf
Washim Akhtar <washimup1@gmail.com>
2025-03-20 13:54:33 +0530

    added api url

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
+import logo from "../assets/logo.png"
 import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../context/StoreContext";
 import {  toast } from 'react-toastify';
@@ -23,7 +24,7 @@ const Navbar = ({ setShowlogin }) => {
       <div className="container w-[90%] mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="flex items-center gap-3">
-          <img src={assets.logo} alt="logo" className="h-10" />
+          <img src={logo} alt="logo" className="h-10" />
         </Link>
 
         {/* Navigation Links */}
